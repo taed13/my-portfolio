2f1065f4786f114feaddb8d5fb9222a7ac652270
taed13 <taedtech13@gmail.com>
2024-02-01 17:24:03 +0700

    done website but not yet modify

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,57 @@
 import React from "react";
 import "./footer.css";
+import { FaFacebookF, FaTwitter, FaInstagram } from "react-icons/fa";
 
 export default function Footer() {
   return (
     <>
-      <div>Footer</div>
+      <footer>
+        <a href="#" className="footer__logo">
+          BAMBOO LEE
+        </a>
+        <ul className="permalinks">
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#experience">Experiences</a>
+          </li>
+          <li>
+            <a href="#services">Services</a>
+          </li>
+          {/* Portfolio  */}
+          <li>
+            <a href="#portfolio">Portfolio</a>
+          </li>
+          {/* testimonials */}
+          <li>
+            <a href="#testimonials">Testimonials</a>
+          </li>
+          {/* contact */}
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+
+        <div className="footer__socials">
+          <a href="https://facebook.com">
+            <FaFacebookF />
+          </a>
+          <a href="https://twitter.com">
+            <FaTwitter />
+          </a>
+          <a href="https://instagram.com">
+            <FaInstagram />
+          </a>
+        </div>
+
+        <div className="footer__copyright">
+          <small>&copy; BAMBOO LEE. All rights reserved.</small>
+        </div>
+      </footer>
     </>
   );
 }
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,10 +1,79 @@
 import React from "react";
 import "./testimonials.css";
+import AVTR1 from "../../assets/avatar1.jpg";
+
+import { Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
 
 export default function Testimonials() {
+  const data = [{}];
   return (
     <>
-      <section id="testimonials">Testimonials</section>
+      <section id="testimonials">
+        <h4>Review from clients</h4>
+        <h2>Testimonials</h2>
+
+        <Swiper
+          className="container testimonials__container"
+          modules={[Pagination]}
+          spaceBetween={40}
+          slidesPerView={1}
+          pagination={{ clickable: true }}
+        >
+          <SwiperSlide className="testimonial">
+            <div className="client__avatar">
+              <img src={AVTR1} alt="avatar" />
+            </div>
+            <h5 className="client__name">Ernest Achiever</h5>
+            <small className="client__review">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum
+              hic quos consequatur similique labore magni excepturi distinctio
+              ducimus esse, voluptates a suscipit repellat in rem ex sit
+              doloremque cumque non.
+            </small>
+          </SwiperSlide>
+          <SwiperSlide className="testimonial">
+            <div className="client__avatar">
+              <img src={AVTR1} alt="avatar" />
+            </div>
+            <h5 className="client__name">Ernest Achiever</h5>
+            <small className="client__review">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum
+              hic quos consequatur similique labore magni excepturi distinctio
+              ducimus esse, voluptates a suscipit repellat in rem ex sit
+              doloremque cumque non.
+            </small>
+          </SwiperSlide>
+          <SwiperSlide className="testimonial">
+            <div className="client__avatar">
+              <img src={AVTR1} alt="avatar" />
+            </div>
+            <h5 className="client__name">Ernest Achiever</h5>
+            <small className="client__review">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum
+              hic quos consequatur similique labore magni excepturi distinctio
+              ducimus esse, voluptates a suscipit repellat in rem ex sit
+              doloremque cumque non.
+            </small>
+          </SwiperSlide>
+          <SwiperSlide className="testimonial">
+            <div className="client__avatar">
+              <img src={AVTR1} alt="avatar" />
+            </div>
+            <h5 className="client__name">Ernest Achiever</h5>
+            <small className="client__review">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum
+              hic quos consequatur similique labore magni excepturi distinctio
+              ducimus esse, voluptates a suscipit repellat in rem ex sit
+              doloremque cumque non.
+            </small>
+          </SwiperSlide>
+        </Swiper>
+      </section>
     </>
   );
 }
