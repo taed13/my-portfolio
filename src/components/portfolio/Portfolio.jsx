883113ab424a1194553e03f7799ae518b8bd41be
taed13 <taedtech13@gmail.com>
2024-02-05 16:35:06 +0700

    update real portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -7,6 +7,8 @@ import IMG4 from "../../assets/portfolio4.jpg";
 import IMG5 from "../../assets/portfolio5.png";
 import IMG6 from "../../assets/portfolio6.jpg";
 
+import { portfolio } from "../../data/Portfolio";
+
 export default function Portfolio() {
   return (
     <>
@@ -14,7 +16,7 @@ export default function Portfolio() {
         <h4>My Recent Work</h4>
         <h2>Portfolio</h2>
 
-        <div className="container portfolio__container">
+        {/* <div className="container portfolio__container">
           <article className="portfolio__item">
             <div className="portfolio__item-image">
               <img src={IMG1} alt="portfolio" />
@@ -159,6 +161,29 @@ export default function Portfolio() {
               </a>
             </div>
           </article>
+        </div> */}
+
+        <div className="container portfolio__container">
+          {portfolio.projects.map((item, index) => (
+            <article className="portfolio__item" key={index}>
+              <div className="portfolio__item-image">
+                <img src={item.urlImage} alt="portfolio" />
+              </div>
+              <h3>{item.title}</h3>
+              <div className="portfolio__item-cta">
+                <a href={item.urlSourceCode} className="btn" target="_blank">
+                  Github
+                </a>
+                <a
+                  href={item.urlDemo}
+                  className="btn btn-primary"
+                  target="_blank"
+                >
+                  Live Demo
+                </a>
+              </div>
+            </article>
+          ))}
         </div>
       </section>
     </>
---
+++ src/data/Portfolio.js
@@ -0,0 +1,20 @@
+export const portfolio = {
+  projects: [
+    {
+      id: 1,
+      title: "Chat App Realtime",
+      urlImage:
+        "https://static.vecteezy.com/system/resources/thumbnails/021/885/976/original/4k-animation-of-chat-mobile-application-for-business-businessman-and-businesswoman-communicate-with-mobile-app-on-big-hand-holding-smart-phone-free-video.jpg",
+      urlSourceCode: "https://github.com/taed13/chatapp",
+      urlDemo: "https://chatapp-ui-roan.vercel.app",
+    },
+    {
+      id: 2,
+      title: "Bamboo Lee Portfolio",
+      urlImage:
+        "https://images.pexels.com/photos/14936128/pexels-photo-14936128.jpeg?cs=srgb&dl=pexels-ann-h-14936128.jpg&fm=jpg",
+      urlSourceCode: "https://github.com/taed13/my-portfolio",
+      urlDemo: "https://bamboolee.vercel.app",
+    },
+  ],
+};
